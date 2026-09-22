6999634928957f302096fa313914d688bfbc263d
MrVirul <virul.mmeemana@gmail.com>
2026-09-22 12:17:14 +0530

    feat: enable discovery and feign clients across services and rename scripts

--- fare-service/src/main/java/com/ridelink/fare_service/FareServiceApplication.java
+++ fare-service/src/main/java/com/ridelink/fare_service/FareServiceApplication.java
@@ -2,8 +2,12 @@ package com.ridelink.fare_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FareServiceApplication {
 
 	public static void main(String[] args) {
